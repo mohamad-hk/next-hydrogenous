ccde6faf553a3f0396682d38b0c0eee697b158ef
mohammad hossein karimi <hm.karimi.23179@gmail.com>
2025-02-09 15:41:07 +0330

    change variables

--- src/app/components/HomePage/Features.js
+++ src/app/components/HomePage/Features.js
@@ -1,5 +1,5 @@
 const Features = async () => {
-  const baseUrl = process.env.hydrogenous_NEXT_PUBLIC_SUPABASE_URL||"http://localhost:3000";
+  const baseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL||"http://localhost:3000";
   const response = await fetch(`${baseUrl}/api/GetFeatures`, {
     cache: "no-store",
   });
