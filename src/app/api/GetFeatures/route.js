fe1be3e7da70b24466074806da3032e15d141f9d
mohammad hossein karimi <hm.karimi.23179@gmail.com>
2025-02-09 16:59:51 +0330

    change table to customer

--- src/app/api/GetFeatures/route.js
+++ src/app/api/GetFeatures/route.js
@@ -2,11 +2,11 @@ import { supabase } from "@/app/utils/client";
 
 export async function GET() {
   try {
-    let { data: tbl_features, error } = await supabase
-      .from("tbl_features")
+    let { data: tbl_customer, error } = await supabase
+      .from("tbl_customer")
       .select("*");
 
-    return Response.json(tbl_features, { status: 200 });
+    return Response.json(tbl_customer, { status: 200 });
   } catch (err) {
     console.error("Error:", err.message); // چاپ خطای کلی
     return Response.json({ error: err.message }, { status: 500 });
--- src/app/components/HomePage/Features.js
+++ src/app/components/HomePage/Features.js
@@ -8,16 +8,14 @@ const Features = async () => {
   }
 
   const features = await response.json();
-
   return (
     <div>
       <h2>Features</h2>
       {features.length > 0 ? (
         features.map((feature) => (
-          <div key={feature.feature_id}>
-            <h3>{feature.feature_heading}</h3>
-            <p>{feature.feature_content}</p>
-            {feature.feature_image && <img src={feature.feature_image} alt={feature.feature_heading} />}
+          <div key={feature.customer_id}>
+            <h3>{feature.first_name}</h3>
+            <p>{feature.last_name}</p>
           </div>
         ))
       ) : (
