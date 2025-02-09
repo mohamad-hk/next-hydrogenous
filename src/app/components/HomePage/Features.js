424a78630ec0ca8d3ffed609b48f421a9f0e8cb7
mohammad hossein karimi <hm.karimi.23179@gmail.com>
2025-02-09 16:42:46 +0330

    change features

--- src/app/components/HomePage/Features.js
+++ src/app/components/HomePage/Features.js
@@ -1,9 +1,22 @@
-const Features = async () => {
-  const response = await fetch(`https://hydrogenous.vercel.app/api/GetFeatures`,);
+const Features = async function () {
+  try {
+    const response = await fetch(
+      "https://hydrogenous.vercel.app/api/GetFeatures",
+      {
+        cache: "no-store", 
+      }
+    );
 
-  if (!response.ok) throw new Error("Failed to fetch features");
+    if (!response.ok) {
+      throw new Error(`Failed to fetch features (Status: ${response.status})`);
+    }
 
-  const features = await response.json();
-  return features
+    const data = await response.json();
+    console.log(data)
+    return data;
+  } catch (error) {
+    console.error("Error fetching features:", error.message);
+  }
 };
+
 export default Features;
