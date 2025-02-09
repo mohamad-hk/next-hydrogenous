e09010860b546742d71caf93d785058f1e858663
mohammad hossein karimi <hm.karimi.23179@gmail.com>
2025-02-09 12:48:36 +0330

    fix url error

--- src/app/components/HomePage/Features.js
+++ src/app/components/HomePage/Features.js
@@ -1,13 +1,11 @@
 const Features = async () => {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
+  const baseUrl = process.env.hydrogenous_NEXT_PUBLIC_SUPABASE_URL || "http://localhost:3000";
   const response = await fetch(`${baseUrl}/api/GetFeatures`, {
     cache: "no-store",
   });
 
-  console.log(response);
   if (!response.ok) throw new Error("Failed to fetch features");
 
   const features = await response.json();
-  console.log(features);
 };
 export default Features;
