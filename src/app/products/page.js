bfcea5fbfef5a95daf27650736b4bf2f36b0ab96
mohammad hossein karimi <hm.karimi.23179@gmail.com>
2025-02-11 23:58:10 +0330

    Findout problem

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -3,16 +3,23 @@ import PersianNumbers from "../utils/ToPersianNumber";
 import { Button } from "@heroui/react";
 
 const ShowProducts = async () => {
-  const response = await fetch(`http://localhost:3000/api/GetProducts`);
+  const response = await fetch(
+    `https://hydrogenous.vercel.app/api/GetProducts`,
+    { cache: "no-store" }
+  );
+
   const products = await response.json();
   return (
     <>
       <div className="grid grid-cols-1 lg:grid-cols-[200px_minmax(900px,_1fr)_20px] xl:grid-cols-[300px_minmax(900px,_1fr)_20px]">
         <div className="w-[20%] md:mt-10 hidden lg:flex ">filter</div>
         <div className="grid grid-cols-1 sm:grid-cols-2 sm:gap-y-5 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-y-5 gap-x-5 sm:my-10">
-          {products.map((product, index) => {
+          {/* {products.map((product, index) => {
             return (
-              <div className="bg-[#f5f7fb] rounded-2xl lg:h-[400px] flex flex-col items-center gap-5 pb-3">
+              <div
+                className="bg-[#f5f7fb] rounded-2xl lg:h-[400px] flex flex-col items-center gap-5 pb-3"
+                key={index}
+              >
                 <Image
                   width={350}
                   height={350}
@@ -30,7 +37,7 @@ const ShowProducts = async () => {
                 </Button>
               </div>
             );
-          })}
+          })} */}
         </div>
       </div>
     </>
