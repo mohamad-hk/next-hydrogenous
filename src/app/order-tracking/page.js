1a191915f1e5d5add3bd2dc8c3e3e87df73f1eb8
mohammad hossein karimi <hm.karimi.23179@gmail.com>
2025-02-27 22:49:36 +0330

    Add orderTracking page

---
+++ src/app/order-tracking/page.js
@@ -0,0 +1,56 @@
+"use client";
+import { Button, Input } from "@heroui/react";
+import { useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
+
+const OrderTracking = () => {
+  const [OrderCode, setOrderCode] = useState("");
+  const [orderStatus, setOrderStatus] = useState("");
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    fetchOrder();
+  }
+  
+  const fetchOrder = async () => {
+    try {
+      const input_param = new URLSearchParams({ order_code: OrderCode });
+
+      const data = await fetch(
+        `https://hydrogenous.vercel.app/api/GetStatusOrder?${input_param}`
+      );
+      if (!data.ok) {
+        throw new Error("Failed to fetch order status");
+      }
+      const response = await data.json();
+      setOrderStatus(response[0].status_order);
+      toast(orderStatus);
+    } catch (error) {
+      console.error("Error fetching order:", error);
+    }
+  };
+
+  return (
+    <>
+      <h2 className="text-2xl ms-5">پیگیری سفارش</h2>
+      <ToastContainer />
+
+      <form onSubmit={handleSubmit} className="my-5">
+        <div className="flex flex-col items-center gap-5 w-[80%] mx-auto sm:w-[60%] md:w-[40%] lg:w-[30%] xl:w-[30%]">
+          <Input
+            label="کد رهگیری"
+            type="text"
+            variant={"bordered"}
+            value={OrderCode}
+            onChange={(e) => setOrderCode(e.target.value)}
+          />
+          <Button type="submit" className=" text-white" color="success">
+            رهگیری سفارش
+          </Button>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default OrderTracking;
