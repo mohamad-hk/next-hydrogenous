580cf2d9d5a2b19e4e20213ab3a118552ec77586
mohammad hossein karimi <hm.karimi.23179@gmail.com>
2025-02-27 23:05:18 +0330

    Api for getting Home slider

---
+++ src/app/api/GetSlider/route.js
@@ -0,0 +1,13 @@
+import { supabase } from "@/app/utils/client";
+
+export async function GET() {
+  try {
+    let { data: tbl_customer, error } = await supabase
+      .from("tbl_sliders")
+      .select("*");
+
+    return Response.json(tbl_customer, { status: 200 });
+  } catch (err) {
+    return Response.json({ error: err.message }, { status: 500 });
+  }
+}
