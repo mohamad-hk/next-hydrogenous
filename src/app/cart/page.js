b30cc6c772ac76ce0723e45a390e44c3a88fd37f
mohammad hossein karimi <hm.karimi.23179@gmail.com>
2025-03-03 22:09:28 +0330

    Create a component for order info

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -1,8 +1,5 @@
-import Link from "next/link";
 import Cart from "../components/CartStore/ShowCart";
-import { Button, Card, CardBody } from "@heroui/react";
-import { Progress } from "@heroui/react";
-import PersianNumbers from "../utils/ToPersianNumber";
+import Orderinfo from "../components/Shipment/Orderinfo";
 
 const Basket = () => {
   return (
@@ -11,36 +8,7 @@ const Basket = () => {
         <div className="shadow-lg rounded-xl p-3">
           <Cart />
         </div>
-        <div>
-          <Card>
-            <CardBody>
-              <div className="flex flex-col gap-5">
-                <div className="flex flex-row justify-between max-h-[400px]">
-                  <div className="flex flex-col items-start gap-2">
-                    <p>هزینه ارسال</p>
-                    <p>مجموع نخفیف</p>
-                    <p>مبلغ قابل پرداخت</p>
-                  </div>
-                  <div className="flex flex-col gap-2">
-                    <p>15000</p>
-                    <p>15000</p>
-                    <p>15000</p>
-                  </div>
-                </div>
-                <div className="flex flex-col gap-3">
-                  <Progress size="md" value={40} />
-                  <p className="text-center">
-                    {PersianNumbers(15000)} تا ارسال رایگان
-                  </p>
-                </div>
-
-                <Button color="primary">
-                  <Link href={"/shipment"}>ادامه خرید</Link>
-                </Button>
-              </div>
-            </CardBody>
-          </Card>
-        </div>
+        <Orderinfo href={"/shipment"} button="ادامه خرید" />
       </div>
     </>
   );
--- src/app/shipment/page.js
+++ src/app/shipment/page.js
@@ -1,44 +1,14 @@
-import { CiMobile2 } from "react-icons/ci";
-import { IoPersonOutline } from "react-icons/io5";
-import { GrLocation } from "react-icons/gr";
-
-import { MdOutlineLocalPostOffice } from "react-icons/md";
-import { CgAdd } from "react-icons/cg";
-import Link from "next/link";
+import Orderinfo from "../components/Shipment/OrderInfo";
+import ShowAddresses from "../components/Shipment/ShowAddresses";
 
 const Shipment = async () => {
   const data = await fetch("https://hydrogenous.vercel.app/api/GetShipment");
   const shipments = await data.json();
   return (
     <>
-      <div className="grid grid-cols-1 gap-y-5 sm:grid-cols-2 sm:gap-x-5 lg:grid-cols-3 lg:gap-x-5  py-10 px-10">
-        {shipments.map((shipment) => {
-          return (
-            <div className="flex flex-col gap-5 rounded-xl shadow-lg p-3 max-w-[500px]">
-              <div className="flex flex-row gap-2">
-                <IoPersonOutline className="text-2xl" />
-                <p>{shipment.f_n_shipment + " " + shipment.l_n_shipment}</p>
-              </div>
-              <div className="flex flex-row gap-2">
-                <CiMobile2 className="text-2xl" />
-                <p>{shipment.phone_shipment}</p>
-              </div>
-              <div className="flex flex-row gap-2">
-                <GrLocation className="text-5xl md:text-4xl " />
-                <p>{shipment.address_shipment}</p>
-              </div>
-              <div className="flex flex-row gap-2">
-                <MdOutlineLocalPostOffice className="text-2xl" />
-                <p>{shipment.zip_code_shipment}</p>
-              </div>
-            </div>
-          );
-        })}
-        <Link href={"#"}>
-          <div className="relative rounded-xl shadow-lg bg-slate-100 max-w-[500px] h-[200px]">
-            <CgAdd className="absolute top-[25%] left-[40%] text-8xl opacity-50" />
-          </div>
-        </Link>
+      <div className="grid mx-auto grid-cols-[_minmax(800px,_1fr)_minmax(100px,_300px)] gap-x-10 p-10 ">
+        <ShowAddresses data={shipments} />
+        <Orderinfo href={"/payment"} button="تایید و تکمیل سفارش" />
       </div>
     </>
   );
