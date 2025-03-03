8654bb3bbc1c045a80f6bf9fc1a31a7d101a3ab0
mohammad hossein karimi <hm.karimi.23179@gmail.com>
2025-03-03 15:38:38 +0330

    Sort by category

--- src/app/api/GetProducts/route.js
+++ src/app/api/GetProducts/route.js
@@ -27,8 +27,8 @@ export async function GET(req) {
       let { data: products, error } = await supabase
         .from("tbl_products")
         .select(
-          "product_id,product_name,product_price,discount_percent,discount_price,product_photo"
-        );
+          "product_id,product_name,product_price,discount_percent,discount_price,product_photo,t_category_id"
+        ).order('t_category_id',{ascending:false});
       return Response.json(products, { status: 200 });
     } catch (error) {
       return Response.json({ error: error.message }, { status: 500 });
