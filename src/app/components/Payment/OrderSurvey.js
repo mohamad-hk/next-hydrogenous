e008dba02da030ba4897c04a37325ef3f27d75e8
mohammad hossein karimi <hm.karimi.23179@gmail.com>
2025-03-06 19:00:06 +0330

    fix logic rendering

--- src/app/components/Payment/OrderSurvey.js
+++ src/app/components/Payment/OrderSurvey.js
@@ -9,24 +9,26 @@ const OrderSurvey = () => {
 
   return (
     <>
-      <div className={`flex flex-row gap-3 my-5 ${cart.length>4?"overflow-x-scroll":null}`}>
+      <div
+        className={`flex flex-row gap-3 my-5 ${
+          cart.length > 4 ? "overflow-x-scroll" : ""
+        }`}
+      >
         {cart.map((item, index) => (
-          <>
-            <div className="flex flex-col items-center" key={index}>
-              <Image
-                src={item.image}
-                width={300}
-                height={300}
-                alt="image not found"
-              />
-              <div className=" flex flex-col items-center lg:flex-row md:items-center md:gap-3">
-                <div className="shadow-md px-3 border border-blue-200 py-1 rounded-md">
-                  {PersianNumbers(item.quantity)} عدد
-                </div>
-                <p className="text-[14px]">{item.name}</p>
+          <div className="flex flex-col items-center" key={index}>
+            <Image
+              src={item.image}
+              width={300}
+              height={300}
+              alt="image not found"
+            />
+            <div className="flex flex-col items-center lg:flex-row md:items-center md:gap-3">
+              <div className="shadow-md px-3 border border-blue-200 py-1 rounded-md">
+                {PersianNumbers(item.quantity)} عدد
               </div>
+              <p className="text-[14px]">{item.name}</p>
             </div>
-          </>
+          </div>
         ))}
       </div>
     </>
