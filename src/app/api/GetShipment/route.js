8de400d8d7040b9c9d477af560d77cdb7d099836
mohammad hossein karimi <hm.karimi.23179@gmail.com>
2025-03-06 18:58:07 +0330

    Change fetching data based on type of id

--- src/app/api/GetShipment/route.js
+++ src/app/api/GetShipment/route.js
@@ -3,14 +3,28 @@ import { supabase } from "@/app/utils/client";
 export async function GET(req) {
   const { searchParams } = new URL(req.url);
   const custId = searchParams.get("cust_id");
-  try {
-    let { data: shipments, error } = await supabase
-      .from("tbl_shipment")
-      .select("*")
-      .eq("cust_id", custId);
+  const ship_id = searchParams.get("ship_id");
+  if (custId) {
+    try {
+      let { data: shipments, error } = await supabase
+        .from("tbl_shipment")
+        .select("*")
+        .eq("customer_id", custId);
 
-    return Response.json(shipments, { status: 200 });
-  } catch (err) {
-    return Response.json({ error: err.message }, { status: 500 });
+      return Response.json(shipments, { status: 200 });
+    } catch (err) {
+      return Response.json({ error: err.message }, { status: 500 });
+    }
+  } else {
+    try {
+      let { data: shipments, error } = await supabase
+        .from("tbl_shipment")
+        .select("*")
+        .eq("shipment_id", ship_id);
+
+      return Response.json(shipments, { status: 200 });
+    } catch (err) {
+      return Response.json({ error: err.message }, { status: 500 });
+    }
   }
 }
