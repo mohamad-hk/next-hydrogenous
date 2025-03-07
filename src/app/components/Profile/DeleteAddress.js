d1ece54b90de4970b19e3f627e897d14eea2dec7
mohammad hossein karimi <hm.karimi.23179@gmail.com>
2025-03-07 14:14:37 +0330

    fix bug

--- src/app/components/Profile/DeleteAddress.js
+++ src/app/components/Profile/DeleteAddress.js
@@ -7,12 +7,44 @@ import {
   useDisclosure,
 } from "@heroui/react";
 
-const DeleteAddress = () => {
+const DeleteAddress = ({ sh_id }) => {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const input_params = new URLSearchParams({
+    ship_id: sh_id,
+  });
 
+  const removeAddress = async (input_params) => {
+    try {
+      const response = await fetch(
+        `https://hydrogenous.vercel.app/api/Shipments/DeleteShipment?${input_params}`,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      const result = await response.json();
+
+      if (response.ok) {
+        console.log("done", result);
+      } else {
+        console.error("Failed to delete shipment:", result.error);
+      }
+    } catch (error) {
+      console.error("Error deleting shipments:", error);
+    }
+  };
+  const deleteData = () => {
+    console.log(1);
+    removeAddress(input_params);
+  };
   return (
     <>
-      <Button color="danger" onPress={onOpen}>حذف آدرس</Button>
+      <Button color="danger" onPress={onOpen}>
+        حذف آدرس
+      </Button>
       <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
         <ModalContent>
           {(onClose) => (
@@ -21,7 +53,13 @@ const DeleteAddress = () => {
                 آیا از حذف آدرس اطمینان دارید؟
               </ModalHeader>
               <ModalFooter>
-                <Button color="danger" onPress={onClose}>
+                <Button
+                  color="danger"
+                  onPress={() => {
+                    onClose;
+                    deleteData();
+                  }}
+                >
                   بله
                 </Button>
                 <Button color="primary" onPress={onClose}>
