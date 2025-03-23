da89f1360c3dc8b0f5abd366dbafc237211274f9
mohammad hossein karimi <hm.karimi.23179@gmail.com>
2025-03-23 23:51:18 +0330

    Change design of loading

--- src/app/components/Loading/Loading.js
+++ src/app/components/Loading/Loading.js
@@ -1,12 +1,10 @@
 "use client";
-import { Spinner } from "@heroui/react";
 
 export default function Loading() {
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-40 flex items-center justify-center z-50">
-      <div className="bg-white p-8 rounded-xl shadow-xl flex flex-col items-center">
-        <Spinner color="primary" className="w-60 md:w-60  lg:w-80 h-16" />
-        <p className="mt-4 text-lg font-semibold text-gray-800">در حال بارگذاری...</p>
+    <div className="fixed inset-0 bg-black bg-opacity-40 backdrop-blur-md flex items-center justify-center z-50">
+      <div className="bg-white backdrop-blur-lg w-80 h-40 rounded-xl shadow-lg flex flex-col items-center justify-center">
+        <div className="loader"></div>
       </div>
     </div>
   );
--- src/app/product/[slug]/loading.js
+++ src/app/product/[slug]/loading.js
@@ -3,7 +3,7 @@
 export default function Loading() {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-40 backdrop-blur-md flex items-center justify-center z-50">
-      <div className="bg-white backdrop-blur-lg w-80 h-40 rounded-xl shadow-lg flex flex-col items-center justify-center">
+      <div className="bg-white  backdrop-blur-lg w-80 h-40 rounded-xl shadow-lg flex flex-col items-center justify-center">
         <div className="loader"></div>
       </div>
     </div>
